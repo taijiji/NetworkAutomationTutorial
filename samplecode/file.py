522ff76b8c58f700036724061b0c93acc148eb4d
Taiji Tsuchiya <tuty0630@gmail.com>
2017-11-12 17:29:26 -0800

    Add tutorial: file input/output

--- samplecode/file.py
+++ samplecode/file.py
@@ -3,7 +3,8 @@ text = file1.read() # ファイル全文を文字列として読み込み
 #text = file1.readline() # ファイル1行目のみを文字列として読み込み
 #text = file1.readlines() # ファイルを行ごとに配列として読み込み
 print(text)
+file1.close()
 
 file2 = open("sample_write.txt", "w")
 file2.write("Good!")
-file2.close
+file2.close()
