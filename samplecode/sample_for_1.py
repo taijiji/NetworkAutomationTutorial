dff3bece1675c99c55d34167999fa9e18af6d1eb
Taiji Tsuchiya <tuty0630@gmail.com>
2018-01-26 10:51:56 +0900

    Separate sample code: from sample_for.py to sample_for_1.py and sample_for_2.py

---
+++ samplecode/sample_for_1.py
@@ -0,0 +1,4 @@
+# 回数を指定して繰り返し処理を実施
+# range(3) = 0,1,2が格納
+for num in range(3): 
+    print(num)
---
+++ samplecode/sample_for_2.py
@@ -0,0 +1,6 @@
+# リスト型変数を利用して繰り返し処理を実施
+router_list = ['router_A', 'router_B']
+
+# router_listの値を、順次 router_name に代入
+for router_name in router_list:
+    print(router_name)
